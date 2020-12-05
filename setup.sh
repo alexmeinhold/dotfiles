9083dbe3ddd42966bcf74b853dc90b9359ad3738
Alexander Meinhold <alexander.meinhold@outlook.com>
2020-12-05 23:17:08 +0100

    add fzf install to setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ dotfiles=$HOME/dotfiles
 
 ln -s $dotfiles/.vimrc $HOME/.vimrc
 ln -s $dotfiles/.gitconfig $HOME/.gitconfig
+
+# setting up fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
