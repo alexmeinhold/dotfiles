87264070c0f448ca2f73c53a3e9dd6a2c614b034
Alexander Meinhold <alexander.meinhold@outlook.com>
2021-01-31 21:20:06 +0100

    remove fzf

--- setup.sh
+++ setup.sh
@@ -4,7 +4,3 @@ dotfiles=$HOME/dotfiles
 
 ln -s $dotfiles/.vimrc $HOME/.vimrc
 ln -s $dotfiles/.gitconfig $HOME/.gitconfig
-
-# setting up fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install
